7275a2b3ebb9116719ad70820c158f328d59c299
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-05-08 12:57:09 +0200

    add negative test

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -11,10 +11,17 @@ const testCasesPLN = [
 
 const testCasesUSD = [
   { amount: '100', expected: '$100.00 = PLN 350.00' },
-  { amount: '200', expected: '$20.00 = PLN 70.00' },
-  { amount: '150', expected: '$200.00 = PLN 700.00' },
-  { amount: '345', expected: '$345.00 = PLN 1207.50' },
+  { amount: '20', expected: '$20.00 = PLN 70.00' },
+  { amount: '200', expected: '$200.00 = PLN 700.00' },
+  { amount: '345', expected: '$345.00 = PLN 1,207.50' },
 ];
+
+const testCases = [
+  { amount: '100', from: 'PLN', to: 'PLN', expected: 'PLN 100.00 = PLN 100.00' },
+  { amount: '65', from: 'PLN', to: 'PLN', expected: 'PLN 65.00 = PLN 65.00' },
+  { amount: '200', from: 'USD', to: 'USD', expected: '$200.00 = $200.00' },
+  { amount: '345', from: 'USD', to: 'USD', expected: '$345.00 = $345.00' },
+]
 
 describe('Component ResultBox', () => {
   it('should render without crashing', () => {
@@ -27,8 +34,34 @@ describe('Component ResultBox', () => {
       render(<ResultBox from="PLN" to="USD" amount={parseInt(testObj.amount)} />);
       const output = screen.getByTestId('amount-output');
       expect(output).toHaveTextContent(testObj.expected);
-      cleanup()
+      cleanup();
     }
   });
+
+  it('should render proper info about conversion when USD -> PLN', () => {  
+    for(const testObj of testCasesUSD) {
+      render(<ResultBox from="USD" to="PLN" amount={parseInt(testObj.amount)} />);
+      const output = screen.getByTestId('amount-output');
+      expect(output).toHaveTextContent(testObj.expected);
+      cleanup();
+    }
+  });
+
+  it('should render proper info about conversion when PLN -> PLN or USD -> USD', () => {  
+    for(const testObj of testCases) {
+      render(<ResultBox from={testObj.from} to={testObj.to} amount={parseInt(testObj.amount)} />);
+      const output = screen.getByTestId('amount-output');
+      expect(output).toHaveTextContent(testObj.expected);
+      cleanup();
+    }
+  });
+
+
+  it('should render "Wrong value..." when value is < 0', () => {  
+    render(<ResultBox from='USD' to='PLN' amount={-1} />);
+    const output = screen.getByTestId('wrong-output');
+    expect(output).toHaveTextContent('Wrong value...');
+    cleanup();
+});
 
 });
